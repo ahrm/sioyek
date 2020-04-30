f179a04087c3aa0c941355db4267ff01ce3aa379
ali <a.hr.mostafavi@gmail.com>
2020-04-30 23:41:06 +0430

    Remove ImGui files.
    Former-commit-id: 9f464d5c1403fcb8ba604c42b51fac99abb5b71d

--- pdf_viewer/ui.h
+++ pdf_viewer/ui.h
@@ -28,138 +28,10 @@
 
 #include <Windows.h>
 #include "utils.h"
-#include "imgui.h"
 
 using namespace std;
 
 const int max_select_size = 100;
-
-class UIWidget {
-public:
-	virtual bool render() = 0;
-};
-
-template<typename T>
-class FilteredSelect : public UIWidget{
-private:
-	vector<wstring> options;
-	vector<T> values;
-	function<void(void*)> on_done;
-	int current_index;
-	bool is_done;
-	char select_string[max_select_size];
-
-public:
-	FilteredSelect(vector<wstring> options, vector<T> values, function<void(void*)> on_done) :
-		options(options), values(values), current_index(0), is_done(false), on_done(on_done) {
-		ZeroMemory(select_string, sizeof(select_string));
-	}
-
-	T* get_value() {
-		int index = get_selected_index();
-		if (index >= 0 && index < values.size()) {
-			return &values[get_selected_index()];
-		}
-		return nullptr;
-	}
-
-	bool is_string_comppatible(wstring incomplete_string, wstring option_string) {
-		incomplete_string = to_lower(incomplete_string);
-		option_string = to_lower(option_string);
-		return option_string.find(incomplete_string) < option_string.size();
-	}
-
-	int get_selected_index() {
-		int index = -1;
-		for (int i = 0; i < options.size(); i++) {
-			if (is_string_comppatible(utf8_decode(select_string), options[i])) {
-				index += 1;
-			}
-			if (index == current_index) {
-				return i;
-			}
-		}
-		return index;
-	}
-
-	wstring get_selected_option() {
-		return options[get_selected_index()];
-	}
-
-	int get_max_index() {
-		int max_index = -1;
-		for (int i = 0; i < options.size(); i++) {
-			if (is_string_comppatible(utf8_decode(select_string), options[i])) {
-				max_index += 1;
-			}
-		}
-		return max_index;
-	}
-
-	void move_item(int offset) {
-		int new_index = offset + current_index;
-		if (new_index < 0) {
-			new_index = 0;
-		}
-		int max_index = get_max_index();
-		if (new_index > max_index) {
-			new_index = max_index;
-		}
-		current_index = new_index;
-	}
-
-	void next_item() {
-		move_item(1);
-	}
-
-	void prev_item() {
-		move_item(-1);
-	}
-
-	bool render() {
-		ImGui::Begin("Select");
-
-		ImGui::SetKeyboardFocusHere();
-		if (ImGui::InputText("search string", select_string, sizeof(select_string), ImGuiInputTextFlags_CallbackHistory | ImGuiInputTextFlags_EnterReturnsTrue,
-			[](ImGuiInputTextCallbackData* data) {
-				FilteredSelect* filtered_select = (FilteredSelect*)data->UserData;
-
-				if (data->EventKey == ImGuiKey_UpArrow) {
-					filtered_select->prev_item();
-				}
-				if (data->EventKey == ImGuiKey_DownArrow) {
-					filtered_select->next_item();
-				}
-
-				return 0;
-			}, this)) {
-			if (is_done == false) {
-				on_done(get_value());
-			}
-			is_done = true;
-		}
-		//ImGui::InputText("search", text_buffer, 100, ImGuiInputTextFlags_CallbackHistory, [&](ImGuiInputTextCallbackData* data) {
-
-		int index = 0;
-		for (int i = 0; i < options.size(); i++) {
-			//if (options[i].find(select_string) == 0) {
-			if (is_string_comppatible(utf8_decode(select_string), options[i])) {
-
-				if (current_index == index) {
-					ImGui::SetScrollHere();
-				}
-
-				if (ImGui::Selectable(utf8_encode(options[i]).c_str(), current_index == index)) {
-					cout << "selected_something" << endl;
-				}
-				index += 1;
-			}
-		}
-
-		ImGui::End();
-		return is_done;
-	}
-};
 
 template<typename T>
 class FilteredSelectWindowClass : public QWidget {
@@ -171,7 +43,6 @@ private:
 	QListView* list_view;
 	vector<T> values;
 	function<void(void*)> on_done;
-
 
 protected:
 	bool eventFilter(QObject* obj, QEvent* event) override {
