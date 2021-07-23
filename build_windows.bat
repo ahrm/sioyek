aa6d11eb0d24f814f9ef2d48b7d2f2087c11299c
ahrm <a.hr.mostafavi@gmail.com>
2021-07-24 03:12:05 +0430

    Copy tutorial.pdf to windows release build directory

--- build_windows.bat
+++ build_windows.bat
@@ -12,5 +12,6 @@ cp pdf_viewer\keys_user.config sioyek-release-windows\keys_user.config
 cp pdf_viewer\prefs.config sioyek-release-windows\prefs.config
 cp pdf_viewer\prefs_user.config sioyek-release-windows\prefs_user.config
 cp -r pdf_viewer\shaders sioyek-release-windows\shaders
+cp tutorial.pdf sioyek-release-windows\tutorial.pdf
 zip -r sioyek-release-windows.zip sioyek-release-windows
 
