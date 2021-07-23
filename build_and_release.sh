efe26a8716028e11e0328747c22d0920253a180e
ahrm <a.hr.mostafavi@gmail.com>
2021-07-23 08:16:43 -0700

    Add linux release script

--- build_and_release.sh
+++ build_and_release.sh
@@ -11,14 +11,15 @@ make USE_SYSTEM_HARFBUZZ=yes
 cd ..
 qmake pdf_viewer_linux.pro
 rm -r AppDir 2> /dev/null
-make install INSTALL_ROOT=AppDir
+make install INSTALL_ROOT=sioyek-release
 
-./linux-deploy-binaries/linuxdeploy-x86_64.AppImage --appdir AppDir --plugin qt
+./linux-deploy-binaries/linuxdeploy-x86_64.AppImage --appdir sioyek-release --plugin qt
 
-cp pdf_viewer/prefs.config AppDir/prefs.config
-cp pdf_viewer/prefs_user.config AppDir/prefs_user.config
-cp pdf_viewer/keys.config AppDir/keys.config
-cp pdf_viewer/keys_user.config AppDir/keys_user.config
-cp -r pdf_viewer/shaders AppDir/shaders
-cp tutorial.pdf AppDir/tutorial.pdf
+cp pdf_viewer/prefs.config sioyek-release/usr/bin/prefs.config
+cp pdf_viewer/prefs_user.config sioyek-release/usr/bin/prefs_user.config
+cp pdf_viewer/keys.config sioyek-release/usr/bin/keys.config
+cp pdf_viewer/keys_user.config sioyek-release/usr/bin/keys_user.config
+cp -r pdf_viewer/shaders sioyek-release/usr/bin/shaders
+cp tutorial.pdf sioyek-release/usr/bin/tutorial.pdf
 
+zip -r sioyek-release.zip sioyek-release
