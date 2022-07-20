ce41830f4bfaf7c42d1023ca29417350dccc81ed
ahrm <a.hr.mostafavi@gmail.com>
2022-07-20 16:19:01 +0430

    Add ability to copy highlights/bookmarks by pressing control+c in menus

--- pdf_viewer/ui.h
+++ pdf_viewer/ui.h
@@ -156,6 +156,10 @@ public:
 		return {};
 	}
 
+	virtual std::wstring get_selected_text() {
+		return L"";
+	}
+
 	bool eventFilter(QObject* obj, QEvent* event) override {
 		if (obj == line_edit) {
 			if (event->type() == QEvent::KeyPress) {
@@ -200,6 +204,13 @@ public:
 				if (key_event->key() == Qt::Key_Backtab) {
 					QKeyEvent* new_key_event = new QKeyEvent(key_event->type(), Qt::Key_Up, key_event->modifiers());
 					QCoreApplication::postEvent(get_view(), new_key_event);
+					return true;
+				}
+				if (((key_event->key() == Qt::Key_C) && is_control_pressed)) {
+					std::wstring text = get_selected_text();
+					if (text.size() > 0) {
+						copy_to_clipboard(text);
+					}
 					return true;
 				}
 				if (key_event->key() == Qt::Key_Return || key_event->key() == Qt::Key_Enter) {
@@ -306,6 +317,7 @@ private:
 
 	QStringListModel* string_list_model = nullptr;
 	std::vector<T> values;
+	std::vector<std::wstring> item_strings;
 	std::function<void(T*)> on_done = nullptr;
 	std::function<void(T*)> on_delete_function = nullptr;
 
@@ -329,6 +341,7 @@ public:
 		on_done(on_done),
 		on_delete_function(on_delete_function)
 	{
+		item_strings = std_string_list;
 		QVector<QString> q_string_list;
 		for (const auto& s : std_string_list) {
 			q_string_list.push_back(QString::fromStdWString(s));
@@ -369,6 +382,18 @@ public:
 			table_view->setWordWrap(true);
 			table_view->verticalHeader()->setSectionResizeMode(QHeaderView::ResizeToContents);
 		}
+	}
+
+	virtual std::wstring get_selected_text(){
+		auto index = this->get_selected_index();
+
+		if (index) {
+
+			auto source_index = this->proxy_model->mapToSource(index.value());
+			return item_strings[source_index.row()];
+		}
+
+		return L"";
 	}
 
 	virtual void on_delete(const QModelIndex& source_index, const QModelIndex& selected_index) override {
