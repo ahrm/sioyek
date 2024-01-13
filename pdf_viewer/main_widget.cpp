cb80499866140193fdad8a04a26151b8c38d5db8
NightMachinery <feraidoonmehri@gmail.com>
2024-01-13 15:27:43 +0330

    MainWidget::wheelEvent: escape+scroll also scrolls horizontally

--- pdf_viewer/main_widget.cpp
+++ pdf_viewer/main_widget.cpp
@@ -2819,9 +2819,12 @@ void MainWidget::wheelEvent(QWheelEvent* wevent) {
 
     bool is_control_pressed = QApplication::queryKeyboardModifiers().testFlag(Qt::ControlModifier) ||
         QApplication::queryKeyboardModifiers().testFlag(Qt::MetaModifier);
+    bool zoom_p = is_control_pressed;
 
     bool is_shift_pressed = QApplication::queryKeyboardModifiers().testFlag(Qt::ShiftModifier);
     bool is_visual_mark_mode = main_document_view->is_ruler_mode() && visual_scroll_mode;
+    bool is_esc_pressed = isKeyPressed(Qt::Key_Escape);
+    bool scroll_horizontally_p = is_shift_pressed || is_esc_pressed;
 
 
 #ifdef SIOYEK_QT6
@@ -2847,7 +2850,7 @@ void MainWidget::wheelEvent(QWheelEvent* wevent) {
         num_repeats = 1;
     }
 
-    if ((!is_control_pressed) && (!is_shift_pressed)) {
+    if ((!zoom_p) && (!scroll_horizontally_p)) {
         if (opengl_widget->is_window_point_in_overview({ normal_x, normal_y })) {
             if (wevent->angleDelta().y() > 0) {
                 scroll_overview(-1);
@@ -2926,12 +2929,12 @@ void MainWidget::wheelEvent(QWheelEvent* wevent) {
         }
     }
 
-    if (is_control_pressed) {
+    if (zoom_p) {
         float zoom_factor = 1.0f + num_repeats_f * (ZOOM_INC_FACTOR - 1.0f);
         zoom(mouse_window_pos, zoom_factor, wevent->angleDelta().y() > 0);
         return;
     }
-    if (is_shift_pressed) {
+    if (scroll_horizontally_p) {
         float inverse_factor = INVERTED_HORIZONTAL_SCROLLING ? -1.0f : 1.0f;
 
         if (wevent->angleDelta().y() > 0) {
