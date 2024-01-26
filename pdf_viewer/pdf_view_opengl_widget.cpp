afd8e57e0bb948683cdae5b02bb49c9fed9ddb2a
ahrm <a.hr.mostafavi@gmail.com>
2024-01-26 13:38:22 +0330

    Fix the issue where abbreviation highlights were not displayed

--- pdf_viewer/pdf_view_opengl_widget.cpp
+++ pdf_viewer/pdf_view_opengl_widget.cpp
@@ -919,6 +919,16 @@ void PdfViewOpenGLWidget::render_overview(OverviewState overview) {
             render_highlight_window(shared_gl_objects.highlight_program, target, HRF_FILL | HRF_BORDER);
         }
     }
+    if (overview_highlights.size() > 0) {
+        glBindBuffer(GL_ARRAY_BUFFER, shared_gl_objects.vertex_buffer_object);
+        glUseProgram(shared_gl_objects.highlight_program);
+        glUniform3fv(shared_gl_objects.highlight_color_uniform_location, 1, config_manager->get_config<float>(L"search_highlight_color"));
+        //glUniform3fv(g_shared_resources.highlight_color_uniform_location, 1, highlight_color_temp);
+        for (auto rect : overview_highlights) {
+            NormalizedWindowRect target = document_to_overview_rect(rect);
+            render_highlight_window(shared_gl_objects.highlight_program, target, HRF_FILL | HRF_BORDER);
+        }
+    }
 
     disable_stencil();
     draw_overview_border();
