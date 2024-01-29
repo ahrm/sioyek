aee802d479256dc4dff2f7fa419aba1e5d9858eb
ahrm <a.hr.mostafavi@gmail.com>
2024-01-29 11:59:58 +0330

    Fix an issue where rendered were not automatically invalidated after executing next_page/prev_page commands (addresses https://github.com/ahrm/sioyek/issues/923#issuecomment-1913808878)

--- pdf_viewer/input.cpp
+++ pdf_viewer/input.cpp
@@ -2357,6 +2357,7 @@ public:
     NextPageCommand(MainWidget* w) : Command(w) {};
     void perform() {
         widget->main_document_view->move_pages(std::max(1, num_repeats));
+        widget->validate_render();
     }
     std::string get_name() {
         return "next_page";
@@ -2369,6 +2370,7 @@ public:
     PreviousPageCommand(MainWidget* w) : Command(w) {};
     void perform() {
         widget->main_document_view->move_pages(std::min(-1, -num_repeats));
+        widget->validate_render();
     }
 
     std::string get_name() {
