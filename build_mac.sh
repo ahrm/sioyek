c5a68f15c2386fdca1a9b614c45f291bf29a017c
Théophile Cailliau <theophile.cailliau@gmail.com>
2024-04-01 20:44:27 +0200

    Update github workflows:
    - Update actions to discard deprecation warnings
    - Use qt6.6.1 everywhere
    - Cache qt installs
    - macOS: Kill XProtect in CI to prevent issue in runner image (see
      linked issue in build_mac.sh for more info)
    - macOS: At compile time, change macOS target to the running system
      (otherwise mupdf and sioyek's macOS targets mismatch)
    - linux: Update dependencies used by mupdf
    - linux: install fuse in CI, required to build AppImage
    - linux: downgrade ubuntu in CI for wider compatibility (linuxdeployqt
      error with recent glibc versions)

--- build_mac.sh
+++ build_mac.sh
@@ -15,6 +15,8 @@ cd mupdf
 make
 cd ..
 
+sed -Ei '' "s/QMAKE_MACOSX_DEPLOYMENT_TARGET.=.[0-9]+/QMAKE_MACOSX_DEPLOYMENT_TARGET = $(sw_vers -productVersion | cut -d. -f1)/" pdf_viewer_build_config.pro
+
 if [[ $1 == portable ]]; then
 	qmake pdf_viewer_build_config.pro
 else
@@ -44,5 +46,11 @@ INFO_PLIST="build/sioyek.app/Contents/Info.plist"
 /usr/libexec/PlistBuddy -c "Add :LSEnvironment dict" "$INFO_PLIST" || echo "LSEnvironment already exists"
 /usr/libexec/PlistBuddy -c "Add :LSEnvironment:PATH string $CURRENT_PATH" "$INFO_PLIST" || /usr/libexec/PlistBuddy -c "Set :LSEnvironment:PATH $CURRENT_PATH" "$INFO_PLIST"
 
+# Hack is required to avoid race condition in macos in CI
+# See https://github.com/actions/runner-images/issues/7522
+if [[ -n "$GITHUB_ACTIONS" ]]; then
+  echo killing...; sudo pkill -9 XProtect >/dev/null || true;
+  echo waiting...; while pgrep XProtect; do sleep 3; done;
+fi
 macdeployqt build/sioyek.app -dmg
 zip -r sioyek-release-mac.zip build/sioyek.dmg
