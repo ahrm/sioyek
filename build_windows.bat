e01c5a4fa49115a2228e523daf092059832cdd34
dtristany <60045737+dtristany@users.noreply.github.com>
2026-08-16 09:52:15 +0100

    fix portable builds on Windows
    NON-PORTABLE was previously hardcoded for all releases

--- build_windows.bat
+++ build_windows.bat
@@ -2,46 +2,52 @@ set BUILD_TYPE=Release
 set BUILD_TYPELC=release
 set PORTABLE=0
 
+:: Parse command line arguments
 for %%a in (%*) do (
     if /i "%%a"=="debug" set BUILD_TYPE=Debug && set BUILD_TYPELC=debug
     if /i "%%a"=="portable" set PORTABLE=1
 )
 
+:: Build MuPDF
 cd mupdf\platform\win32\
 msbuild -maxcpucount mupdf.sln /m /property:Configuration=%BUILD_TYPE% /property:MultiProcessorCompilation=true
 cd ..\..\..
 
+:: Build Zlib
 cd zlib
 nmake -f win32/makefile.msc
 cd ..
 
-:: Configure qmake dynamically based on our build type
-if "%BUILD_TYPE%"=="Debug" (
-    qmake -tp vc "DEFINES+=NON_PORTABLE" "CONFIG+=debug" pdf_viewer_build_config.pro
-) else (
-    qmake -tp vc "DEFINES+=NON_PORTABLE" "CONFIG+=release" pdf_viewer_build_config.pro
-)
+:: Set portable preprocessor flag dynamically based on %PORTABLE%
+set QMAKE_DEFINES="DEFINES+=NON_PORTABLE"
+if "%PORTABLE%"=="1" set QMAKE_DEFINES=
 
+:: Generate Visual Studio project via qmake
+qmake -tp vc %QMAKE_DEFINES% "CONFIG+=%BUILD_TYPELC%" pdf_viewer_build_config.pro
+
+:: Build Sioyek executable
 msbuild -maxcpucount sioyek.vcxproj /m /property:Configuration=%BUILD_TYPE%
-rm -r sioyek-release-windows 2> NUL
+
+:: Clean and recreate output staging directory
+if exist sioyek-release-windows rmdir /s /q sioyek-release-windows
 mkdir sioyek-release-windows
-:: Note: MSBuild outputs to a folder named after the configuration (debug or release lower case)
+
 cp %BUILD_TYPELC%\sioyek.exe sioyek-release-windows\sioyek.exe
 cp pdf_viewer\keys.config sioyek-release-windows\keys.config
 cp pdf_viewer\prefs.config sioyek-release-windows\prefs.config
 cp -r pdf_viewer\shaders sioyek-release-windows\shaders
 cp tutorial.pdf sioyek-release-windows\tutorial.pdf
 
+:: Deploy Qt dependencies
 windeployqt --qmldir ./pdf_viewer/touchui --%BUILD_TYPELC% sioyek-release-windows\sioyek.exe
-REM windeployqt sioyek-release-windows\sioyek.exe
 
-REM Overwrite Qt's default vcruntime with the secure ones pulled from System32
-cp windows_runtime\vcruntime140.dll sioyek-release-windows\vcruntime140.dll
-cp windows_runtime\vcruntime140_1.dll sioyek-release-windows\vcruntime140_1.dll
+:: Copy Runtime DLLs if present
+if exist windows_runtime\vcruntime140.dll copy /y windows_runtime\vcruntime140.dll sioyek-release-windows\
+if exist windows_runtime\vcruntime140_1.dll copy /y windows_runtime\vcruntime140_1.dll sioyek-release-windows\
 
-REM Feed Qt6 the OpenSSL 3 DLLs it expects to find at runtime
-cp openssl_build\bin\libssl-3-x64.dll sioyek-release-windows\libssl-3-x64.dll
-cp openssl_build\bin\libcrypto-3-x64.dll sioyek-release-windows\libcrypto-3-x64.dll
+:: Copy OpenSSL 3 DLLs if present
+if exist openssl_build\bin\libssl-3-x64.dll copy /y openssl_build\bin\libssl-3-x64.dll sioyek-release-windows\
+if exist openssl_build\bin\libcrypto-3-x64.dll copy /y openssl_build\bin\libcrypto-3-x64.dll sioyek-release-windows\
 
 if "%PORTABLE%"=="1" (
     cp pdf_viewer\keys_user.config sioyek-release-windows\keys_user.config
